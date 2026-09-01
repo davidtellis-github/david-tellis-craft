3e2cabb115cebc3906ee8abe9b3730ccf397baa3
David Tellis <davidtellis12@gmail.com>
2026-09-01 21:22:14 +0530

    value sec

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -12,15 +12,15 @@ const tabsData: {
 }, {
   key: "designers",
   label: "Product Designers",
-  headline: <span style={{ color: "#b8b9ab" }}>"I care about the final screens. But mostly, I care about why they ended up looking like that. "</span>
+  headline: <span style={{ color: "#b8b9ab" }}>I care about the final screens. But mostly, I care about why they ended up looking like that. </span>
 }, {
   key: "pms",
   label: "Founders",
-  headline: <span style={{ color: "#b8b9ab" }}>"I design products by figuring out what actually needs solving."</span>
+  headline: <span style={{ color: "#b8b9ab" }}>I design products by figuring out what actually needs solving.</span>
 }, {
   key: "engineers",
   label: "Engineers",
-  headline: <span style={{ color: "#b8b9ab" }}>"I've studied computers and practicing design, so I get both worlds."</span>
+  headline: <span style={{ color: "#b8b9ab" }}>I've studied computers and practicing design, so I get both worlds.</span>
 }];
 
 const Hero: React.FC = () => {
